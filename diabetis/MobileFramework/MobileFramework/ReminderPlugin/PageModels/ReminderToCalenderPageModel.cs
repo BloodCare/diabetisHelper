a4df254a58162398d3bacac9d3474d11ee818669
Ankit Dixit <ankit.dixit@stud.hochschule-heidelberg.de>
2016-05-29 12:25:14 +0200

    Add reminder to calendar screen

---
+++ diabetis/MobileFramework/MobileFramework/ReminderPlugin/PageModels/ReminderToCalenderPageModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using MobileFramework.PluginManager;
+using Xamarin.Forms;
+
+namespace MobileFramework.ReminderPlugin
+{
+	public class ReminderToCalenderPageModel : FreshMvvm.FreshBasePageModel
+	{
+		public ReminderToCalenderPageModel ()
+		{
+			
+		}
+
+		public Command onCancel
+		{
+			get
+			{
+				//test notification
+				return new Command( (value) =>
+					{
+						CoreMethods.PushPageModel<ReminderListPageModel> (value);
+					});
+			}
+		}
+
+		public Command onSave
+		{
+			get
+			{
+				//test notification
+				return new Command( (value) =>
+					{
+						CoreMethods.PushPageModel<ReminderListPageModel> (value);
+					});
+			}
+		}
+	}
+}
+
+
