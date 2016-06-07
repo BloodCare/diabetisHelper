9717d857e63420c079a1d4f9910ae3881ace905b
Mukhtar Hussain <mukhtar.hussain@stud.hochschule-heidelberg.de>
2016-06-07 11:03:50 +0200

    Framework setup for Unit Tesing

---
+++ diabetis/MobileFrameWork.Tests/Models/Reminders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MobileFrameWork.Tests.Models
+{
+    public class RemindersTest
+    {
+        [Fact]
+        public void MustBeTrue()
+        {
+            Assert.Equal(1, 1);
+        }
+    }
+}
