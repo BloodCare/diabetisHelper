8a874ccc8693a4d534a45bc2c6079daeb5690018
Alpana <alpana.chaphalkar@stud.hochschule-heidelberg.de>
2016-06-18 00:51:54 +0200

    Create, Read, Update and Delete opration defined for SQLLite DB

--- diabetis/MobileFramework/MobileFramework/ReminderPlugin/Services/ReminderDataService.cs
+++ diabetis/MobileFramework/MobileFramework/ReminderPlugin/Services/ReminderDataService.cs
@@ -1,63 +1,68 @@
 ﻿using System;
 using System.Threading.Tasks;
 using System.Collections.Generic;
-
+using MobileFramework.Database;
 namespace MobileFramework.ReminderPlugin
 {
-    public class ReminderDataService : IReminderDataService
-    {
-        private static List<Reminder> _reminderList;
-        private static ReminderDataService _reminderDataServ;
+	public class ReminderDataService : IReminderDataService
+	{
+		private static List<Reminder> _reminderList;
+		private static ReminderDataService _reminderDataServ;
+		private BloodCareDatabase _bloodCareDatabase;
 
-        private ReminderDataService()
-        {
-            _reminderList = new List<Reminder>();
-            this.Initilize();
-        }
-        public void Initilize()
-        {
-            this.LoadReminders();
-        }
-        void LoadReminders()
-        {
-            AddReminder(new Reminder { Name = "Blood Sugar", Description = "Any description can be there", isActive = false });
-            AddReminder(new Reminder { Name = "Medicine", Description = "Any description can be there", isActive = true });
-            AddReminder(new Reminder { Name = "Emergency", Description = "Any description can be there", isActive = true });
-            AddReminder(new Reminder { Name = "Physical", Description = "Any description can be there", isActive = false });
-        }
+		private ReminderDataService()
+		{
+			_reminderList = new List<Reminder>();
+			_bloodCareDatabase = new BloodCareDatabase();
+			this.Initilize();
+		}
+		public void Initilize()
+		{
+			this.LoadReminders();
+		}
+		void LoadReminders()
+		{
+			_reminderList.Clear();
+			foreach (var item in _bloodCareDatabase.GetItems<Reminder>())
+			{
+				_reminderList.Add(item);
+			}
+
+		}
 
 
 
-        public static ReminderDataService ReminderDataServ
-        {
-            get
-            {
-                if (_reminderDataServ == null)
-                    _reminderDataServ = new ReminderDataService();
-                return _reminderDataServ;
-            }
-        }
+		public static ReminderDataService ReminderDataServ
+		{
+			get
+			{
+				if (_reminderDataServ == null)
+					_reminderDataServ = new ReminderDataService();
+				return _reminderDataServ;
+			}
+		}
 
-        public bool AddReminder(Reminder reminder)
-        {
-            _reminderList.Add(reminder);
-            return true;
-        }
+		public bool AddReminder(Reminder reminder)
+		{
+			_bloodCareDatabase.SaveItem(reminder);
+			LoadReminders();
+			return true;
+		}
 
-        public async Task<List<Reminder>> GetReminders()
-        {
+		public async Task<List<Reminder>> GetReminders()
+		{
 
-            await Task.Delay(TimeSpan.FromSeconds(0));
-            return _reminderList;
+			await Task.Delay(TimeSpan.FromSeconds(0));
+			return _reminderList;
 
-        }
+		}
 
-        public bool RemoveReminder(Reminder reminder)
-        {
-            _reminderList.Remove(reminder);
-            return true;
+		public bool RemoveReminder(Reminder reminder)
+		{
+			_bloodCareDatabase.DeleteItem(reminder);
+			LoadReminders();
+			return true;
 
-        }
-    }
+		}
+	}
 }
-
